7b1d2c04b9e55986b714418b3f6774da9a2ba110
Nicholas <npdullam@gmail.com>
2025-02-04 17:36:37 -0500

    clerk provider

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,7 +8,7 @@ if (!process.env.DATABASE_URL)
 
 export default {
   schema: "./db/schema.ts",
-  out: "./drizzle",
+  out: "./db/drizzle",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
