ca665b45370876e3a3ec0168dfaedc3535a359b2
Nicholas <npdullam@gmail.com>
2025-02-04 18:35:03 -0500

    indices

--- db/schema.ts
+++ db/schema.ts
@@ -1,26 +1,36 @@
 import { sql } from "drizzle-orm";
-import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, unique } from "drizzle-orm/pg-core";
 
-export const Applications = pgTable("applications", {
-  id: text("id")
-    .primaryKey()
-    .notNull()
-    .default(sql`gen_random_uuid()`),
-  userId: text("userId").notNull(),
-  createdAt: timestamp("createdAt").defaultNow().notNull(),
-  updatedAt: timestamp("updatedAt").defaultNow().notNull(),
-  status: text("status").notNull().default("pending"),
-  companyName: text("companyName").notNull(),
-  jobTitle: text("jobTitle").notNull(),
-  jobUrl: text("jobUrl").notNull(),
-});
+export const Applications = pgTable(
+  "applications",
+  {
+    id: text("id")
+      .primaryKey()
+      .notNull()
+      .default(sql`gen_random_uuid()`),
+    userId: text("userId").notNull(),
+    createdAt: timestamp("createdAt").defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt").defaultNow().notNull(),
+    status: text("status").notNull().default("pending"),
+    companyName: text("companyName").notNull(),
+    jobTitle: text("jobTitle").notNull(),
+    jobUrl: text("jobUrl").notNull(),
+  },
+  (t) => [unique().on(t.jobUrl)],
+);
 
-export const Documents = pgTable("documents", {
-  id: text("id")
-    .primaryKey()
-    .notNull()
-    .default(sql`gen_random_uuid()`),
-  type: text("type").notNull(),
-  url: text("url").notNull(),
-  applicationId: text("applicationId").references(() => Applications.id),
-});
+export const Documents = pgTable(
+  "documents",
+  {
+    id: text("id")
+      .primaryKey()
+      .notNull()
+      .default(sql`gen_random_uuid()`),
+    name: text("name").notNull(),
+    type: text("type").notNull(),
+    url: text("url").notNull(),
+    userId: text("userId").notNull(),
+    applicationId: text("applicationId").references(() => Applications.id),
+  },
+  (t) => [unique().on(t.name, t.userId)],
+);
