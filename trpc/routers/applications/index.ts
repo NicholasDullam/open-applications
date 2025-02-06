ab6f96ca361a0869135ba612cbbc04ec9b4b4f69
Nicholas <npdullam@gmail.com>
2025-02-05 19:34:50 -0500

    document, applications create routes

--- trpc/routers/applications/index.ts
+++ trpc/routers/applications/index.ts
@@ -2,10 +2,19 @@ import { db } from "@/db";
 import { applications } from "@/db/schema";
 import { authProcedure } from "@/trpc/procedures/auth";
 import { and, eq } from "drizzle-orm";
+import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
 import { router } from "../../trpc";
 
 export const applicationsRouter = router({
+  create: authProcedure
+    .input(createInsertSchema(applications).omit({ userId: true }))
+    .mutation(async ({ ctx: { userId }, input }) => {
+      return await db.insert(applications).values({
+        ...input,
+        userId,
+      });
+    }),
   get: authProcedure
     .input(z.string())
     .query(async ({ ctx: { userId }, input }) => {
--- trpc/routers/documents/index.ts
+++ trpc/routers/documents/index.ts
@@ -2,10 +2,19 @@ import { db } from "@/db";
 import { documents } from "@/db/schema";
 import { authProcedure } from "@/trpc/procedures/auth";
 import { and, eq } from "drizzle-orm";
+import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
 import { router } from "../../trpc";
 
 export const documentsRouter = router({
+  create: authProcedure
+    .input(createInsertSchema(documents).omit({ userId: true }))
+    .mutation(async ({ ctx: { userId }, input }) => {
+      return await db.insert(documents).values({
+        ...input,
+        userId,
+      });
+    }),
   get: authProcedure.input(z.string()).query(async ({ ctx, input }) => {
     return await db
       .select()
