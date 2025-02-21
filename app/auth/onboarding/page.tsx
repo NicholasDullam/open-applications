c4adb534b28b7ad23f804678e07d8b38ba9f9669
Nicholas <npdullam@gmail.com>
2025-02-20 22:31:06 -0500

    basic user onboarding

--- app/auth/onboarding/page.tsx
+++ app/auth/onboarding/page.tsx
@@ -1,7 +1,19 @@
+import { api } from "@/trpc/server";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
 export default async function Page() {
-  return (
-    <div>
-      <h1>Onboarding</h1>
-    </div>
-  );
+  const user = await currentUser();
+  try {
+    if (!user) throw new Error("User not found");
+    await api.users.create({
+      email: user.emailAddresses[0].emailAddress,
+      firstName: user.firstName ?? "",
+      lastName: user.lastName ?? "",
+      externalId: user.id,
+    });
+  } catch (error) {
+    console.error(error);
+    redirect("/");
+  }
 }
--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -2,11 +2,13 @@ import { authProcedure } from "../procedures/auth";
 import { mergeRouters, router } from "../trpc";
 import { applicationsRouter } from "./applications";
 import { documentsRouter } from "./documents";
+import { usersRouter } from "./users";
 
 export const appRouter = mergeRouters(
   router({
     applications: applicationsRouter,
     documents: documentsRouter,
+    users: usersRouter,
     greeting: authProcedure.query(({ ctx: { user } }) => {
       return `Hello ${user.firstName}!`;
     }),
